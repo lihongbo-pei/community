1905f00140bc69340850d3f803b5f9d5d77b9bd8
xiaoli <1973742240@qq.com>
2021-03-06 21:29:59 +0800

    SpringSecurity实现权限控制

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 社区网站
-牛客网项目
+# 社区网站——牛客网项目
+这是一个比较综合的Java项目，采用SpringBoot框架，整合了Redis、Kafka、ElasticSearch和SpringSecurity。
 正在学习中，代码同步更新
 
 ElasticSearch部分的代码因为使用的版本不同，代码会与视频中老师讲的不同，自己重新coding完成实现功能。
--- src/main/java/com/lhb/community/controller/MessageController.java
+++ src/main/java/com/lhb/community/controller/MessageController.java
@@ -146,8 +146,8 @@ public class MessageController implements CommunityConstant {
         User user = hostHolder.getUser();
         // 查询评论类通知
         Message message = messageService.findLatestNotice(user.getId(), TOPIC_COMMENT);
-        Map<String, Object> messageVo = new HashMap<>();
         if (message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message",message);
             String content = HtmlUtils.htmlUnescape(message.getContent());
             HashMap data = JSONObject.parseObject(content, HashMap.class);
@@ -162,13 +162,13 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_COMMENT);
             messageVo.put("unread", unread);
+            model.addAttribute("commentNotice", messageVo);
         }
-        model.addAttribute("commentNotice", messageVo);
 
         // 查询点赞类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_LIKE);
-        messageVo = new HashMap<>();
         if (message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message",message);
             String content = HtmlUtils.htmlUnescape(message.getContent());
             HashMap data = JSONObject.parseObject(content, HashMap.class);
@@ -183,12 +183,13 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_LIKE);
             messageVo.put("unread", unread);
+            model.addAttribute("likeNotice", messageVo);
         }
-        model.addAttribute("likeNotice", messageVo);
+
         // 查询关注类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_FOLLOW);
-        messageVo = new HashMap<>();
         if (message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message",message);
             String content = HtmlUtils.htmlUnescape(message.getContent());
             HashMap data = JSONObject.parseObject(content, HashMap.class);
@@ -202,8 +203,8 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_FOLLOW);
             messageVo.put("unread", unread);
+            model.addAttribute("followNotice", messageVo);
         }
-        model.addAttribute("followNotice", messageVo);
 
         // 查询未读消息数量
         int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
